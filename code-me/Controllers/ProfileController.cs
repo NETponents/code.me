eb930a5d4f6dc29cf64673b0dc3b47606d454ac4
ARMmaster17 <wordman05@gmail.com>
2015-06-08 13:27:03 -0400

    Added values to ViewBag for user profile page.

--- code-me/Controllers/ProfileController.cs
+++ code-me/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Octokit;
 
 namespace code_me.Controllers
 {
@@ -15,6 +16,9 @@ namespace code_me.Controllers
         }
         public ActionResult UserDetails()
         {
+            ViewData.Add("user_name", "ARMmaster17");
+            ViewData.Add("user_image", "https://avatars3.githubusercontent.com/u/4678601?v=3&s=460");
+            
             return View();
         }
     }
