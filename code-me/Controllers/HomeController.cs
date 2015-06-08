c083684a549623fba22d7613bcbd941159153010
ARMmaster17 <wordman05@gmail.com>
2015-06-08 09:41:20 -0400

    Added login check for issue #1

--- code-me/Controllers/HomeController.cs
+++ code-me/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace code_me.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-            if isLoggedin()
+            if (User.Identity.Name != "")
             {
                 return View();
             }
