02da8bbf5e4bddcacecb8a2c0a26db4244c8aa41
ARMmaster17 <wordman05@gmail.com>
2015-06-08 21:19:48 -0400

    Added viewbag items for Profile/UserDetails

--- code-me/Controllers/ProfileController.cs
+++ code-me/Controllers/ProfileController.cs
@@ -16,9 +16,16 @@ namespace code_me.Controllers
         }
         public ActionResult UserDetails()
         {
-            ViewData.Add("user_name", "ARMmaster17");
-            ViewData.Add("user_image", "https://avatars3.githubusercontent.com/u/4678601?v=3&s=460");
+            Activity[] user_activityfeed = new Activity[20];
+            List<string> user_orgs = new List<string>();
             
+            ViewData.Add("user_name", "user_name");
+            ViewData.Add("user_image", "https://avatars3.githubusercontent.com/u/4678601?v=3&s=460");
+            ViewData.Add("user_joindate", DateTime.Now.ToString());
+            ViewData.Add("user_feeditems", user_activityfeed);
+            ViewData.Add("user_organizations", user_orgs);
+            ViewData.Add("user_contact_email", "user_contact_email");
+            ViewData.Add("user_contact_website", "user_contact_website");
             return View();
         }
     }
