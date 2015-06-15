4806db323fa16d0c5a906aa353ce5d6671d6064a
Joshua Zenn <wordman05@gmail.com>
2015-06-15 15:30:28 -0400

    Create levels.rb

---
+++ levels.rb
@@ -0,0 +1,41 @@
+function getLevelName(lnum)
+  if lnum == 0
+    return 'Unknown'
+  elsif lnum == 1
+    return 'Hello World!'
+  elsif lnum == 2
+    return 'Modder'
+  elsif lnum == 3
+    return 'Coffee Intern'
+  elsif lnum == 4
+    return 'Tutorial Reader'
+  elsif lnum == 5
+    return 'Google-fu Master'
+  elsif lnum == 6
+    return 'CS101 Sensei'
+  elsif lnum == 7
+    return 'Commit Factory'
+  else
+    return 'Ultimate Coder'
+  end
+end
+function getLevelFromPoints(points)
+  if points >= 0 and points < 100
+    return 1
+  elsif points >= 100 and points < 200
+    return 2
+  elsif points >= 200 and points < 400
+    return 3
+  elsif points >= 400 and points < 800
+    return 4
+  elsif points >= 800 and points < 1600
+    return 5
+  elsif points >= 1600 and points < 3200
+    return 6
+  elsif points >= 3200 and points < 6400
+    return 7
+  elsif points >= 6400
+    return 8
+  else
+    return 0
+end
