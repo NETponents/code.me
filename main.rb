517f94817d413c28f103b4e96376193e62fc5448
Joshua Zenn <wordman05@gmail.com>
2015-06-15 16:03:23 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'slim'
+require_relative 'inc/levels'
 
 set :port, ENV['PORT'] || 8080
 set :bind, ENV['IP'] || '0.0.0.0'
@@ -22,48 +23,6 @@ helpers do
   def auth_username
     return session[:username]
   end
-end
-
-def getLevelName(lnum)
-  if lnum == 0
-    return 'Unknown'
-  elsif lnum == 1
-    return 'Hello World!'
-  elsif lnum == 2
-    return 'Modder'
-  elsif lnum == 3
-    return 'Coffee Intern'
-  elsif lnum == 4
-    return 'Tutorial Reader'
-  elsif lnum == 5
-    return 'Google-fu Master'
-  elsif lnum == 6
-    return 'CS101 Sensei'
-  elsif lnum == 7
-    return 'Commit Factory'
-  else
-    return 'Ultimate Coder'
-  end
-end
-def getLevelFromPoints(points)
-  if points >= 0 and points < 100
-    return 1
-  elsif points >= 100 and points < 200
-    return 2
-  elsif points >= 200 and points < 400
-    return 3
-  elsif points >= 400 and points < 800
-    return 4
-  elsif points >= 800 and points < 1600
-    return 5
-  elsif points >= 1600 and points < 3200
-    return 6
-  elsif points >= 3200 and points < 6400
-    return 7
-  elsif points >= 6400
-    return 8
-  else
-    return 0
 end
 
 get '/' do
