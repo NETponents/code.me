8604f72433e2d3766cd7ac8ace39f4bef831eb58
Joshua Zenn <wordman05@gmail.com>
2015-06-20 13:43:26 -0400

    Update versiontest.rb

--- tests/versiontest.rb
+++ tests/versiontest.rb
@@ -1,1 +1,17 @@
+require_relative "../test_helper"
+require_relative "../inc/builddata"
+require_relative "../inc/pagevars"
+require "test/unit"
+require 'rack/test'
+require_relative '../main'
 
+class TestVersion < Test::Unit::TestCase
+  include Rack::Test::Methods
+  def app
+    Sinatra::Application
+  end
+  def test_homepage
+    get '/'
+    assert last_response.ok?
+  end
+end
