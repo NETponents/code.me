01223ee6fc138e1aad0b564abce50205743979db
Joshua Zenn <wordman05@gmail.com>
2015-06-22 20:03:01 -0400

    Update api.rb

--- inc/api.rb
+++ inc/api.rb
@@ -3,4 +3,7 @@ module API
   def API.a()
     return "200 API online. Version: #{Pagevars.setVars("CIbuild")}"
   end
+  def API.aNotifications(uname)
+    return "<info><item type=\"info\" title=\"#{uname}\">Notification API is online.</item></info>"
+  end
 end
