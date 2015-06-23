622bb54419a605fc9b28fd48c89306e79a969854
Joshua Zenn <wordman05@gmail.com>
2015-06-23 14:57:25 -0400

    Update versiontest.rb
    Extended unit testing for ~/inc/levels.rb

--- test/versiontest.rb
+++ test/versiontest.rb
@@ -2,8 +2,27 @@ require_relative "../test_helper"
 require "test/unit"
 require 'rack/test'
 require_relative '../main'
+require_relative '../inc/levels'
 
 class TestVersion < Test::Unit::TestCase
+  def test_unit_levels
+    i = 0
+    r = ""
+    while r != "Ultimate Coder" do
+      Levels.getLevelName(i)
+      i++
+    end
+    assert r != ""
+  end
+  def test_unit_levelsnum
+    i = 100
+    r = 100
+    while r != 0 do
+      r = Levels.getLevelFromPoints(i)
+      i = i + 100
+    end
+    assert r == 0
+  end
   include Rack::Test::Methods
   def app
     Sinatra::Application
